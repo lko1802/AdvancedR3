8bca0c6bb2771a50a76c64c33fae8199869c9ff5
Lærke Karner Overgaard <YMMU7H@rsyd.dk>
2025-12-03 15:54:41 +0100

    Ran the simple model

--- R/functions.R
+++ R/functions.R
@@ -42,3 +42,20 @@ clean <- function(data) {
     dplyr::summarise(value = mean(value), .groups = "keep") |>
     dplyr::ungroup()
 }
+
+
+
+
+#' Preprocessing the data
+#'
+#' @param data  The lipidomics data
+#'
+#' @returns A.data.frame
+#
+preprocess <- function(data){
+  data |>
+    dplyr::mutate(
+      class = as.factor(class),
+      value = scale(value)
+    )
+}
