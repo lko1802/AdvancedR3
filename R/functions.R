1f67c48195ad23318f31a3797c2d3c294da4bc55
Lærke Karner Overgaard <YMMU7H@rsyd.dk>
2025-12-03 10:52:50 +0100

    Added a plot of distribution

--- R/functions.R
+++ R/functions.R
@@ -9,7 +9,7 @@ create_table_descriptive_stats <- function(data) {
   data |>
     dplyr::group_by(metabolite) |>
     dplyr::summarise(dplyr::across(value, list(mean = mean, sd = sd))) |>
-    dplyr::mutate(dplyr::across(where(is.numeric), \(x) round(x, digits = 1))) |>
+    dplyr::mutate(dplyr::across(tidyselect::where(is.numeric), \(x) round(x, digits = 1))) |>
     dplyr::mutate(MeanSD = glue::glue("{value_mean} ({value_sd})")) |>
     dplyr::select(Metabolite = metabolite, "Mean SD" = MeanSD)
 }
--- _targets.R
+++ _targets.R
@@ -9,7 +9,7 @@ library(tarchetypes) # Load other packages as needed.
 
 # Set target options:
 tar_option_set(
-  #packages = c("tibble") # Packages that your targets need for their tasks.
+  # packages = c("tibble") # Packages that your targets need for their tasks.
   # format = "qs", # Optionally set the default storage format. qs is fast.
   #
   # Pipelines that take a long time to run may benefit from
@@ -58,10 +58,15 @@ list(
   tar_target(
     name = lipidomics,
     command = readr::read_csv(file,
-                              show_col_types = FALSE)
+      show_col_types = FALSE
+    )
   ),
   tar_quarto(
     name = quarto_doc,
     path = "docs/learning.qmd"
+  ),
+  tar_target(
+    name = table_descriptive_stats,
+    command = create_table_descriptive_stats(lipidomics)
   )
 )
